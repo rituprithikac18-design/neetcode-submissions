b624a17c86a8d22c8834a6ceff81a02f185d049a
Ritu Chandrasekar <rituprithika.c18@gmail.com>
2026-06-16 19:07:06 +0530

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        n=len(matrix)
+        m=len(matrix[0])
+        left,right=0,m-1
+        top,bottom=0,n-1
+        a=[]
+        while(left<=right and top<=bottom):
+            for i in range(left,right+1,+1):
+                a.append(matrix[top][i])
+            top+=1
+            for i in range(top,bottom+1,+1):
+                a.append(matrix[i][right])
+            right-=1
+            if top<=bottom:
+                for i in range(right,left-1,-1):
+                    a.append(matrix[bottom][i])
+                bottom-=1
+            if left<=right:
+                for i in range(bottom,top-1,-1):
+                    a.append(matrix[i][left])
+                left+=1
+        return a
+
+
