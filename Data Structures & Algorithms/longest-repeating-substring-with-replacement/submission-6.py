79d259486c654d51d06695ec8b8e477a770bad37
Ritu Chandrasekar <rituprithika.c18@gmail.com>
2026-06-27 19:35:41 +0530

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l,r,maxlen,maxf=0,0,0,0
+        hashh=[0]*26
+        while r<len(s):
+            hashh[ord(s[r])-ord('A')]+=1
+            maxf=max(maxf,hashh[ord(s[r])-ord('A')])
+            if r-l+1-maxf>k:
+                hashh[ord(s[l])-ord('A')]-=1
+                l+=1
+            if r-l+1-maxf<=k:
+                maxlen=max(maxlen,r-l+1)
+            r+=1
+        return maxlen
